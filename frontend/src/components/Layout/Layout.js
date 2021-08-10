39e933cd716efef7ea02c10cb4ebc53ed67abdd8
Wasswa Richard <wasswa@awamo.com>
2021-08-10 18:19:18 +0300

    project setup

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -3,13 +3,19 @@ import { withRouter } from "react-router-dom";
 import ReactGA from "react-ga";
 import Navbar from "../Navbar/Navbar";
 import Sidebar from "../Sidebar/Sidebar";
+import LoginForm from "../LoginForm/LoginForm";
+import SignUpForm from "../SignUpForm/SignUpForm";
 const Layout =  ({children}) => {
     const isNavbarVisible = true;
     useEffect(() => {
         ReactGA.pageview(window.location.pathname + window.location.search);
     });
+
+
     return (
         <>
+            {/*<LoginForm/>*/}
+            {/*<SignUpForm/>*/}
             <Navbar/>
             <Sidebar/>
             <div id="content" className={!isNavbarVisible ? "active" : ""}>
