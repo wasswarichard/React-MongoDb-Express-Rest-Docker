89e3e1779d9ae90a1fe3ec4c2beb91eaf285e73c
Wasswa Richard <wasswa@awamo.com>
2021-08-14 14:10:11 +0300

    project setup

--- frontend/src/components/Todos/Todos.js
+++ frontend/src/components/Todos/Todos.js
@@ -7,7 +7,16 @@ import useTodoSearch from "./useTodoSearch";
 import TodoItem from "./TodoItem";
 import {store} from "../../state/store/store";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '30ch',
+    },
+  },
+  submit: {
+        margin: theme.spacing(3, 0, 2),
+  },
   addTodoContainer: { padding: 10 },
   addTodoButton: { marginLeft: 5 },
   todosContainer: { marginTop: 10, padding: 10 },
@@ -30,7 +39,7 @@ const useStyles = makeStyles({
   deleteTodo: {
     visibility: "hidden",
   },
-});
+}));
 
 function Todos() {
   const authentication = store.getState().session
@@ -71,7 +80,8 @@ function Todos() {
     setTodos(items);
   }
 
-  function addNewTodo() {
+  function addNewTodo(event) {
+    event.preventDefault();
     fetch(`${config.apiUrl}/api/todo`, {
       headers: {
         Accept: "application/json",
@@ -105,30 +115,43 @@ function Todos() {
                 setPageNumber(1);
               }}/>
             </Typography>
-            <Paper className={classes.addTodoContainer}>
-              <Box display="flex" flexDirection="row">
-                <Box flexGrow={2}>
+
+            <Paper>
+              <form className={classes.root} onSubmit={addNewTodo}>
+                <div>
                   <TextField
                       required
-                      label="text"
-                      fullWidth
+                      id="filled-text"
+                      label="Text"
+                      variant="filled"
                       value={text}
                       onChange={(event) => setText(event.target.value)}
                   />
                   <TextField
                       required
+                      id="filled-due-date"
                       label="Due Date"
                       type="date"
                       value={dueDate}
+                      variant="filled"
                       InputLabelProps={{
                         shrink: true,
                       }}
                       defaultValue="2021-05-24"
                       onChange={event => setDueDate(event.target.value)}
                   />
-                </Box>
-                <Button className={classes.addTodoButton} startIcon={<Icon>add</Icon>} onClick={() => addNewTodo()}>Add</Button>
-              </Box>
+
+                  <Button
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      className={classes.submit}
+                      startIcon={<Icon>add</Icon>}
+                  >
+                    Add
+                  </Button>
+                </div>
+              </form>
             </Paper>
             <DragDropContext onDragEnd={handleOnDragEnd}>
               <Droppable droppableId="characters">
