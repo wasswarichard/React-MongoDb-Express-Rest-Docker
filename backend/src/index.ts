d13c98d50de1c3334616097d407648ae9357c4e5
Wasswa Richard <wasswa@awamo.com>
2021-08-14 16:41:28 +0300

    project setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,29 +1,16 @@
 import express from 'express';
 import cors from 'cors';
-import { createServer } from "http";
-import { Server } from "socket.io";
 import config from 'config';
 import log from "./logger";
 import connect from "./db/connect";
 import routes from "./routes/routes";
 import { deserializeUser } from "./middleware";
-// import socket from "./socket";
 
 const port = config.get('port') as number;
 const host = config.get('host') as string;
 
 const app = express();
-app.use(cors())
-
-//
-// const httpServer = createServer(app);
-//
-// const io = new Server(httpServer, {
-//     cors: {
-//         origin: corsOrigin,
-//         credentials: true,
-//     },
-// });
+app.use(cors());
 
 app.use(deserializeUser);
 
@@ -34,6 +21,4 @@ app.listen(port, host, () => {
     connect();
     log.info(`Server listening at http://${host}:${port}`);
     routes(app);
-
-    // socket({ io });
 })
